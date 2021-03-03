036be0059d6af551688a87d1140a915e8d5b87f9
Craig Minihan <craig@ripcordsoftware.com>
2021-03-03 13:44:22 +0000

    Added help to hvk8sctl, included config output to file

---
+++ src/hyper-v/scripts/arguments.ps1
@@ -0,0 +1,51 @@
+$ErrorActionPreference = "Stop"
+
+class ArgumentOption {
+    [string] $key = $null
+    [object] $values = @()
+
+    [void] SetKey([string] $key) { $this.key = $key -replace '^--', '' }
+    [void] AppendValue([string] $value) { $this.values += $value }
+
+    [bool] HasKey() { return !!$this.key }
+    [bool] HasValues() { return $this.values.Count }
+
+    [object] GetObject() {
+        [object] $obj = $null
+        if ($this.HasKey()) {
+            if ($this.HasValues()) {
+                $obj = @{ $this.key = $this.values -join ' ' }
+            } else {
+                $obj = @{ $this.key = $true }
+            }
+        }
+        return $obj
+    }
+}
+
+class Arguments {
+    static [object] GetLongOptions([object] $options) {
+        [object] $optObj = @{}
+        [object] $currentOption = [ArgumentOption]::new()
+
+        $options | Where-Object { !!$_ } | ForEach-Object {
+            if ($_.StartsWith('--')) {
+                if ($currentOption.HasKey()) {
+                    $optObj += $currentOption.GetObject()
+                    $currentOption = [ArgumentOption]::new()
+                }
+                $currentOption.SetKey($_)
+            } else {
+                $currentOption.AppendValue($_)
+            }
+        } | Out-Null
+
+        if ($currentOption.HasKey()) {
+            $optObj += $currentOption.GetObject()
+        }
+
+        return $optObj
+    }
+}
+
+[type] $script:Arguments = &{ return [Arguments] }
