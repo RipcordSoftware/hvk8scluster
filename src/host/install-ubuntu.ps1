289870fbe3416196103386f76fd1bd28c32bc043
Craig Minihan <craig@ripcordsoftware.com>
2021-03-10 23:09:17 +0000

    Install Ubuntu without needing the store ... also allow the user to specify username and password

--- src/host/install-ubuntu.ps1
+++ src/host/install-ubuntu.ps1
@@ -1,44 +1,67 @@
 param (
+    [string] $user = 'hvk8s',
+    [string] $password = 'hvk8s',
     [string] $version = '1804',
-    [switch] $skipDownload
+    [switch] $skipDownload,
+    [switch] $skipStore
 )
 
 $ErrorActionPreference = 'Stop'
 $ProgressPreference = 'SilentlyContinue'
 
+[string] $downloadDir = "~\Downloads"
+[string] $archivePath = "${downloadDir}\Ubuntu-${version}.appx"
+[string] $tempDistroDir = "$($env:TEMP)\hvk8s\wsl\ubuntu${version}"
+[string] $archiveZipPath = "${tempDistroDir}\Ubuntu-${version}.zip"
+
 if (!$skipDownload) {
-    Invoke-WebRequest -Uri https://aka.ms/wsl-ubuntu-${version} -OutFile ~\Downloads\Ubuntu-${version}.appx -UseBasicParsing
+    Write-Host "Downloading the Linux distribution package..."
+    Invoke-WebRequest -Uri https://aka.ms/wsl-ubuntu-${version} -OutFile $archivePath -UseBasicParsing
 }
 
-Add-AppxPackage ~\Downloads\Ubuntu-${version}.appx
+New-item -Path $tempDistroDir -ItemType Directory -Force
 
-[object] $packages = Get-AppxPackage
+if ($skipStore) {
+    Write-Host "Unpacking the distribution package..."
+    Copy-Item -Path $archivePath -Destination $archiveZipPath
+    Expand-Archive -Path $archiveZipPath -DestinationPath $tempDistroDir -Force
+}
+else {
+    Write-Host "Registering the package as an application..."
 
-[object] $ubuntu = $packages | 
-    Where-Object { $_ -and $_.InstallLocation } | 
-    Where-Object {
-        [string] $manifestPath = Join-Path -Path $_.InstallLocation -ChildPath "AppxManifest.xml"
-        if (Test-Path $manifestPath) {
-            [object] $doc = [System.Xml.XmlDocument]::new()
-            [object] $tr = [System.Xml.XmlTextReader]::new($manifestPath)
-            try {
-                $tr.Namespaces = $false
-                $doc.Load($tr)
-            } finally {
-                $tr.Close()
+    Add-AppxPackage $archivePath
+
+    # get the path to the package so we can run it for the first time
+    [object] $packages = Get-AppxPackage    
+    [object] $ubuntu = $packages | 
+        Where-Object { $_ -and $_.InstallLocation } | 
+        Where-Object {
+            [string] $manifestPath = Join-Path -Path $_.InstallLocation -ChildPath "AppxManifest.xml"
+            if (Test-Path $manifestPath) {
+                [object] $doc = [System.Xml.XmlDocument]::new()
+                [object] $tr = [System.Xml.XmlTextReader]::new($manifestPath)
+                try {
+                    $tr.Namespaces = $false
+                    $doc.Load($tr)
+                } finally {
+                    $tr.Close()
+                }
+
+                [object] $node = $doc.DocumentElement.SelectSingleNode("/Package/Applications/Application[@Id='ubuntu${version}']")
+                return $node
             }
+        } | Select-Object -First 1
 
-            [object] $node = $doc.DocumentElement.SelectSingleNode("/Package/Applications/Application[@Id='ubuntu${version}']")
-            return $node
-        }
-    } | Select-Object -First 1
-
-[string] $tempDistroPath = "$($env:TEMP)\hvk8s\wsl\ubuntu${version}"
-New-item -Path $tempDistroPath -ItemType Directory -Force
-Copy-Item -Path "$($ubuntu.InstallLocation)\ubuntu${version}.exe" -Destination $tempDistroPath -Force
-Copy-Item -Path "$($ubuntu.InstallLocation)\install.tar.gz" -Destination $tempDistroPath -Force
+    Copy-Item -Path "$($ubuntu.InstallLocation)\ubuntu${version}.exe" -Destination $tempDistroDir -Force
+    Copy-Item -Path "$($ubuntu.InstallLocation)\install.tar.gz" -Destination $tempDistroDir -Force
+}
 
 [string] $tempFile = New-TemporaryFile
-"hvk8s`nhvk8s`nhvk8s`nexit`n" | Out-File -Encoding ascii -NoNewline -FilePath $tempFile
+try {
+    "${user}`n${password}`n${password}`nexit`n" | Out-File -Encoding ascii -NoNewline -FilePath $tempFile
 
-Start-Process -RedirectStandardInput $tempFile -FilePath "${tempDistroPath}\ubuntu${version}.exe" -Wait -NoNewWindow
+    Write-Host "Running the distribution for the first time..."
+    Start-Process -RedirectStandardInput $tempFile -FilePath "${tempDistroDir}\ubuntu${version}.exe" -Wait -NoNewWindow
+} finally {
+    Remove-Item -Path $tempFile -Force -ErrorAction SilentlyContinue
+}
