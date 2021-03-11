790c25ca04cf2e820497aa8cff3976559942119e
Craig Minihan <craig@ripcordsoftware.com>
2021-03-11 00:21:16 +0000

    Restart after WSL is installed

--- src/host/install-wsl.ps1
+++ src/host/install-wsl.ps1
@@ -1,5 +1,15 @@
 #Requires -RunAsAdministrator
 
+param (
+    [switch] $disableRestart
+)
+
 $ErrorActionPreference = 'Stop'
 
 Enable-WindowsOptionalFeature -Online -FeatureName Microsoft-Windows-Subsystem-Linux -NoRestart
+
+if (!$disableRestart) {
+    Write-Host "Restarting computer in 10s, press CTRL+C to abort..."
+    Start-Sleep 10
+    Restart-Computer -Force
+}
