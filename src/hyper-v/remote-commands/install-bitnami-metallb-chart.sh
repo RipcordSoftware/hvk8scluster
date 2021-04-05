6987a997a9c691c9b38450d8d96608eb3cae1f5f
Craig Minihan <craig@ripcordsoftware.com>
2021-04-05 22:00:51 +0100

    Install metallb to metallb-system

--- src/hyper-v/remote-commands/install-bitnami-metallb-chart.sh
+++ src/hyper-v/remote-commands/install-bitnami-metallb-chart.sh
@@ -7,8 +7,14 @@ set -o nounset
 
 helm repo add bitnami https://charts.bitnami.com/bitnami
 
+NS_METALLB=$(kubectl get ns metallb-system || true)
+if [ -z "${NS_METALLB}" ]; then
+    kubectl create namespace metallb-system
+fi
+
 # values: https://github.com/bitnami/charts/blob/master/bitnami/metallb/values.yaml
 helm upgrade -i \
+    -n metallb-system \
     --set configInline.address-pools[0].name=generic-cluster-pool \
     --set configInline.address-pools[0].protocol=layer2 \
     --set configInline.address-pools[0].addresses[0]=172.31.0.100-172.31.0.200 \
